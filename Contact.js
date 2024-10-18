8df9b0bb70a9a20e955d62491bbdd9acfea5b70f
ariyan-3cyber <jahin.ariyan23@gmail.com>
2024-10-18 21:14:50 +1100

    Updated Contact component to display name and address

--- Contact.js
+++ Contact.js
@@ -1,49 +1,15 @@
-import { useState, useEffect } from 'react';  // import useEffect
-import PhoneList from './PhoneList.js';
+import React from 'react';
 
-function Contact(props) {
-    const {contact, contacts, setContacts} = props;
-    const [expanded, setExpanded] = useState(false);
-    const [phones, setPhones] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost/api/contacts/' + contact.id + '/phones')
-            .then(response => response.json())
-            .then(data => setPhones(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
-
-    const expandStyle = {
-        display: expanded ? 'block' : 'none'
-    };
-
-    async function doDelete(e) {
-        e.stopPropagation();
-
-        const response = await fetch('http://localhost/api/contacts/' + contact.id, {
-            method: 'DELETE',
-        });
-
-        let newContacts = contacts.filter((c) => {
-            return c.id !== contact.id;
-        });
-
-        setContacts(newContacts);
-    }
-
+function Contact({ contact, onClick }) {
     return (
-        <div key={contact.id} className='contact' onClick={(e) => setExpanded(!expanded)}>
-            <div className='title'>
+        <div className="contact" onClick={() => onClick(contact)}>
+            <div className="title">
                 <h3>{contact.name}</h3>
-                <button className='button red' onClick={doDelete}>Delete Contact</button>
             </div>
-
-            <div style={expandStyle}>
-                <hr />
-                <PhoneList phones={phones} setPhones={setPhones} contact={contact} />
-            </div>
+            <p>{contact.address}</p>
+            <button className="button red" onClick={(e) => { e.stopPropagation(); contact.doDelete(contact.id); }}>
+                Delete Contact
+            </button>
         </div>
     );
 }
